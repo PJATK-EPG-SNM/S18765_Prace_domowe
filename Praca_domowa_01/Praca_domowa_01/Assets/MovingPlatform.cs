c47329b82d3b99989a77fed8de06c4bb41194eac
s18765 <61684711+s18765@users.noreply.github.com>
2020-03-14 22:30:24 +0100

    Praca_domowa_01 v.0.2
    Added moving platform

---
+++ Praca_domowa_01/Praca_domowa_01/Assets/MovingPlatform.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingPlatform : MonoBehaviour
+{
+    public float speed;
+    public float reset;
+
+    public Transform movingPlatform, position1, position2;
+    public Vector2 newPosition;
+    public string currentState; 
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Change();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movingPlatform.position = Vector2.Lerp(movingPlatform.position, newPosition, speed * Time.deltaTime);
+
+    }
+
+    void Change()
+    {
+        if (currentState == "Moving to 1")
+        {
+            currentState = "Moving to 2";
+            newPosition = position2.position;
+        }
+        else if (currentState == "Moving to 2")
+        {
+            currentState = "Moving to 1";
+            newPosition = position1.position;
+        }
+    
+        else if (currentState == "")
+        { 
+            currentState = "Moving to 2";
+            newPosition = position2.position;
+        }
+        Invoke("Change", reset);
+    }
+
+}
