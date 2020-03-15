d45b8538ddeebab16dc9c5a04f5bdf7b0e8c961a
s18765 <61684711+s18765@users.noreply.github.com>
2020-03-15 20:48:09 +0100

    Praca_domowa_01 v.0.3
    Added disappearing platform

---
+++ Praca_domowa_01/Praca_domowa_01/Assets/Disappear.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Disappear : MonoBehaviour
+{
+    public GameObject Player;
+    public float time = 2;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject == Player)
+        {
+            Destroy(this.gameObject, time);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.gameObject == Player)
+        { }
+    }
+
+
+
+}
